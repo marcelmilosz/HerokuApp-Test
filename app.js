2818ba8d7640a863e2489ef9e449343ca8ae3517
Marcel Milosz <marcel.milosz99@gmail.com>
2022-09-03 13:53:48 +0200

    Heroku stuff 1

--- app.js
+++ app.js
@@ -37,7 +37,7 @@ app.use(express.urlencoded({ extended: true }));
 
 
 app.get('/', (req, res) => {
-    res.render("index.ejs", { mongoKey: mongoDB })
+    res.render("index.ejs")
 })
 
 /* Login */
